86a0eec62f6410022274f5c8c36d2c674a4928a8
Beni312 <bence312@gmail.com>
2018-04-09 11:45:30 +0200

    table expand animation -> matRowExpand constant

--- src/app/modules/shared/components/mat.row.expand.directive.ts
+++ src/app/modules/shared/components/mat.row.expand.directive.ts
@@ -1,4 +1,11 @@
 import {Directive, HostBinding, HostListener, Input, TemplateRef, ViewContainerRef} from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+
+export const SlideRowAnimation = trigger('detailExpand', [
+  state('void', style({height: '0px', minHeight: '0', visibility: 'hidden'})),
+  state('*', style({height: '*', visibility: 'visible'})),
+  transition('void <=> *', animate('250ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+]);
 
 @Directive({
   selector: '[matRowExpand]'
--- src/app/modules/user/components/submission/submission.component.ts
+++ src/app/modules/user/components/submission/submission.component.ts
@@ -1,23 +1,17 @@
 import { ActivatedRoute } from '@angular/router';
-import { AfterViewInit, Component, OnInit, QueryList, TemplateRef, ViewChild, ViewChildren } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
 import { Submission } from '../../../../models/submission';
 import { SubmissionPreloadResponse } from '../../../../models/submission.preload.response';
 import { SubmissionService } from '../../../../services/submission.service';
 import { ToasterService } from 'angular5-toaster/dist';
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import { SlideRowAnimation } from '../../../shared/components/mat.row.expand.directive';
 
 @Component({
   selector: 'app-submission',
   templateUrl: './submission.component.html',
   styleUrls: ['./submission.component.scss'],
-  animations: [
-    trigger('detailExpand', [
-      state('void', style({height: '0px', minHeight: '0', visibility: 'hidden'})),
-      state('*', style({height: '*', visibility: 'visible'})),
-      transition('void <=> *', animate('250ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
-    ]),
-  ]
+  animations: [SlideRowAnimation]
 })
 export class SubmissionComponent implements OnInit, AfterViewInit {
 
@@ -27,7 +21,6 @@ export class SubmissionComponent implements OnInit, AfterViewInit {
 
   @ViewChild(MatSort) sort: MatSort;
   @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChildren(TemplateRef) rows: QueryList<TemplateRef<any>>;
 
   constructor(private submissionService: SubmissionService,
               private activatedRoute: ActivatedRoute,
