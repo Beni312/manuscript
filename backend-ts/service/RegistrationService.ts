e3426da7b9ef1768d57533d9f4e0608b99e797b2
Beni <bence312@gmail.com>
2019-06-13 15:48:52 +0200

    fix register create: set academic disciplines, set password to user

--- backend-ts/service/RegistrationService.ts
+++ backend-ts/service/RegistrationService.ts
@@ -7,7 +7,7 @@ import { RoleEnum } from "../model/enum/RoleEnum";
 @injectable()
 export class RegistrationService {
 
-  async create(params: any): Promise<User> {
+  async create(params: any): Promise<void> {
     if (params.password.password != params.password.passwordAgain) {
       throw new RegistrationError("Password parity check failed. The given passwords are not matched.");
     }
@@ -19,20 +19,18 @@ export class RegistrationService {
     }
 
     const role = await Role._findOne<Role>({where: {name: RoleEnum.AUTHOR}});
-    const user: User  = await User.create({
+    await User.create({
       roleId: role.id,
       title: params.user.title,
       firstName: params.user.firstName,
       lastName: params.user.lastName,
-      email: params.user.email,
-      academicDisciplines: params.user.academicDisciplines
+      email: params.user.email
+    }).then(async (user: User) => {
+      await user.setAcademicDisciplines(params.academicDisciplines.map(item => item.id));
+      await UserAlias.create({userId: user.id, username: params.user.username});
+
+      const salt = bcrypt.genSaltSync();
+      await Password.create({password: bcrypt.hashSync(params.password.password, salt), salt: salt, userId: user.id});
     });
-
-    await UserAlias.create({userId: user.id, username: params.user.username});
-
-    const salt = bcrypt.genSaltSync();
-    await Password.create({password: bcrypt.hashSync(params.password.password, salt), salt: salt});
-
-    return user;
   }
 }
