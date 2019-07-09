9923904a6c87fa7ba1e49c7f0119d6a41828bc37
Beni <bence312@gmail.com>
2019-07-09 10:14:18 +0200

    deploy error fix

--- src/app/modules/user/components/permission.handler.ts
+++ src/app/modules/user/components/permission.handler.ts
@@ -2,7 +2,7 @@ import { UserService } from '../../../services/user.service';
 
 export class PermissionHandler {
 
-  protected hasPermission(roles: string[]): boolean {
+  public hasPermission(roles: string[]): boolean {
     if (!UserService.isLogined() || !roles.includes((UserService.getPreload().role.toUpperCase()))) {
       return false;
     }
